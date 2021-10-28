69be4b25f11b4f171dc8553f56e2dcb4fd059710
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2021-10-28 16:49:36 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const PLAYER_KEY = 'videoplayer - current - time';
+
+function onPlay(data) {
+  const time = data.seconds;
+  localStorage.setItem(PLAYER_KEY, time);
+}
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function getTime() {
+  const saveTime = localStorage.getItem(PLAYER_KEY);
+
+  player
+    .setCurrentTime(saveTime)
+    .then()
+    .catch(function (error) {
+      return error.name;
+    });
+}
+player.on('play', throttle(getTime, 1000));
